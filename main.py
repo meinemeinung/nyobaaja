ce48b69bb7d7228f616965b0148cbbd790444313
rickyising <104302682+rickyising@users.noreply.github.com>
2022-04-28 17:18:13 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 17:17:01 2022
+
+@author: Pinnacle
+"""
+
+def main():
+    n = 10
+    for i in range(n):
+        print("*" * i)
+
+if __name__=='__main__':
+    main()
