d01ed7437f3b8c4646edde6a06e75a71bd253b86
rickyising <rickytheising@gmail.com>
2022-04-28 17:44:06 +0700

    what
    why

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nyobaaja
+
+## SUITSWIWWWW
---
+++ mamamia.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 17:40:30 2022
+
+@author: Pinnacle
+"""
+
+def mamamia():
+    for i in range(10):
+        print("mamamia")
